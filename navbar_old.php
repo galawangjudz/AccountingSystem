957d12116f9b23f9a7f482cd7e9ed7d4b371f9cd
galawangjudz <jaevoli@gmail.com>
2022-12-20 09:44:38 +0800

    add navbar based on user roles

--- navbar_old.php
+++ navbar_old.php
@@ -1,9 +1,4 @@
  <!-- Left side column. contains the logo and sidebar -->
- <?php 
- 
- $usertype = $_SESSION['user_type'];
-
-?>
  <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
@@ -14,10 +9,20 @@
         <li class="treeview">
           <a href="index.php?page=dashboard"><i class="fa fa-th"></i><span>Dashboard</span></a>
         </li>
-        <?php if ($usertype == 'IT Admin' || $usertype == 'Agent' || $usertype == 'SOS' || $usertype == 'COO'){ ?>
-        <li class="treeview">
-          <a href="index.php?page=customer-list"><i class="fa fa-users"></i> <span>Clients</span></a>
-        </li>
+        <!-- Menu 1.1 -->
+     <!--    <li class="treeview">
+          <a href="#"><i class="fa fa-file-invoice"></i><span>Pending List</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="index.php?page=csr-list"><i class="fa fa-map-location-dot"></i>Pending</a></li>
+            <li><a href="#"><i class="fa fa-square"></i>Approved</a></li>
+            <li><a href="#"><i class="fa fa-house"></i>Disapproved</a></li>
+            <li><a href="#"><i class="fa fa-house"></i>Cancelled</a></li>
+          </ul>
+        </li> -->
         <li class="treeview">
           <a href="index.php?page=csr-list"><i class="fa-solid fa-map-location-dot"></i><span> Pending</span></a>
         </li>
@@ -25,22 +30,15 @@
          <li class="treeview">
           <a href="index.php?page=ra-list"><i class="fa-solid fa-book"></i><span>  RA List</span></a>
         </li>
-        <?php } ?>
          <!-- Menu 1 -->
-         <?php if ($usertype == 'IT Admin' || $usertype == 'CA Supervisor'){ ?>
          <li class="treeview">
           <a href="index.php?page=ca-list"><i class="fa-solid fa-book"></i><span>  CA List</span></a>
         </li>
-        <?php } ?>
-       
-        <?php if ($usertype == 'IT Admin' || $usertype == 'Cashier'){ ?>
-             <!-- Menu 1.2 -->
+    
+          <!-- Menu 1.2 -->
         <li class="treeview">
           <a href="index.php?page=reservation-list"><i class="fa fa-calendar-check"></i><span>Reservation</span></a>
         </li>
-
-        <?php } ?>
-        <?php if ($usertype == 'IT Admin'){ ?>
         <!-- Menu 2 -->
          <li class="treeview">
           <a href="#"><i class="fa fa-clipboard-list"></i><span>Inventory</span>
@@ -54,6 +52,11 @@
             <li><a href="index.php?page=house-list"><i class="fa fa-house"></i>Manage House</a></li>
           </ul>
         </li>
+        <!-- Menu 3 -->
+        <li class="treeview">
+          <a href="index.php?page=customer-list"><i class="fa fa-users"></i> <span>Clients</span></a>
+        </li>
+        
         <!-- Menu 4 -->
         <li class="treeview">
           <a href="index.php?page=user-list"><i class="fa fa-user"></i><span>System Users</span></a>
@@ -66,10 +69,9 @@
           </a>
           <ul class="treeview-menu">
             <li><a href="index.php?page=agent-list"><i class="fa fa-id-card-clip"></i>Manage Agents</a></li>
+       <!--      <li><a href="index.php?page=commission-list"><i class="fa fa-money-bill-1-wave"></i>Commissions</a></li> -->
           </ul>
-    
-        </li>      
-        <?php } ?>  
+        </li>        
       </ul>
       <!-- /.sidebar-menu -->
     </section>
